bac7083251ee098ebe9c5c759a41171c71bf9ae8
VitorSirotenco <vitorsirotenco33@gmail.com>
2022-12-19 17:07:12 -0300

    final copa do mundo

--- mains.js
+++ mains.js
@@ -24,10 +24,7 @@ document.querySelector("#app").innerHTML = `
             <img src="./assets/logo.svg" alt="Logo NWL">
             </header>
             <main id="cards">  
-            ${creatCard("03/12", "sabado", creatGame("unitedstates", "12:00", "netherlands") + creatGame("argentina", "16:00", "australia"))}
-            ${creatCard("04/12", "domingo", creatGame("france", "12:00", "poland") + creatGame("senegal", "16:00", "england") )}
-            ${creatCard("05/12", "segunda", creatGame("japan", "12:00", "croatia") + creatGame("brazil", "16:00", "south-korea"))}
-            ${creatCard("06/12", "terça", creatGame("morocco", "12:00", "spain") + creatGame("portugal", "16:00", "switzerland"))}             
+            ${creatCard("18/12", "domingoo", creatGame("argentina", "12:00", "france") )}         
             </main>
 ` 
 
